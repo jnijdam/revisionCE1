496b081117d5bdef179678d4dc245b74c830aa81
Jan Nijdam <nijdam.jan@gmail.com>
2023-10-31 14:29:23 +0100

    Maj liste lettre muette

---
+++ Scripts/ordre_alphabétique.ps1
@@ -0,0 +1,99 @@
+﻿# Importer la bibliothèque nécessaire pour l'interface graphique
+Add-Type -AssemblyName System.Windows.Forms
+
+# Initialiser les variables globales
+$score = 0
+$round = 0
+
+# Liste des mots
+$words = "abricot", "banane", "cerise", "datte", "framboise", "grenade", "kiwi", "litchi", "mangue", "nectarine", "olive", "pomme", "raisin", "tangerine", "uma", "vanille", "wakame", "xérès", "yuzu", "zatte",
+          "balançoire", "bonbon", "câlin", "doudou", "école", "fable", "goûter", "histoire", "île", "jouet", "kermesse", "luge", "manège", "nounou", "ours", "poupée", "quenotte", "récréation", "sucette", "tricycle",
+          "arithmétique", "bureau", "cahier", "dictée", "équerre", "fournitures", "géométrie", "instituteur", "jeu", "leçon", "maîtresse", "numération", "orthographe", "poésie", "question", "réponse", "salle", "tableau", "univers", "vocabulaire"
+
+
+# Fonction pour démarrer un nouveau tour
+function New-Round {
+    $global:word1 = $words | Get-Random
+    $global:word2 = $words | Get-Random
+    # Assurez-vous que les deux mots sont différents
+    while ($global:word1 -eq $global:word2) {
+        $global:word2 = $words | Get-Random
+    }
+    $global:label.Text = "Quel mot vient en premier dans l'ordre alphabétique ?`n1. $global:word1`n2. $global:word2"
+    $global:textBox.Text = ""
+    $global:textBox.Focus()
+    $global:round++
+}
+
+# Fonction pour vérifier la réponse
+function Check-Answer {
+    $answer = $global:textBox.Text
+    $correct = 0
+    if ($global:word1.CompareTo($global:word2) -lt 0) {
+        $correct = 1
+    } else {
+        $correct = 2
+    }
+
+    if ($answer -eq $correct) {
+        $global:labelResult.Text = "Correct !"
+        $global:score++
+    } else {
+        $global:labelResult.Text = "Incorrect ! Le mot $correct. $($global:word1) vient avant $($global:word2)"
+    }
+
+    if ($global:round -lt 10) {
+        New-Round
+    } else {
+        $global:label.Text = "Jeu terminé ! Votre score est $global:score sur 10."
+        $global:textBox.Enabled = $false
+        $ReplayButton.Visibility = "Visible"  # Affiche le bouton lorsque le jeu est terminé
+    }
+}
+
+# Créer et configurer le formulaire principal
+$mainForm = New-Object System.Windows.Forms.Form
+$mainForm.Text = "Test d'alphabétisation"
+$mainForm.Size = New-Object System.Drawing.Size(600,400)
+$mainForm.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::Sizable
+
+# Créer et configurer les contrôles
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,10)
+$label.Size = New-Object System.Drawing.Size(560,120)
+$label.Font = New-Object System.Drawing.Font($label.Font.FontFamily, 20)
+$mainForm.Controls.Add($label)
+
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(10,140)
+$textBox.Size = New-Object System.Drawing.Size(560,40)
+$textBox.Font = New-Object System.Drawing.Font($textBox.Font.FontFamily, 20)
+$textBox.Add_KeyPress({
+    if ($_.KeyChar -eq [System.Windows.Forms.Keys]::Enter) {
+        $_.Handled = $true
+        Check-Answer
+    }
+})
+$mainForm.Controls.Add($textBox)
+
+$labelResult = New-Object System.Windows.Forms.Label
+$labelResult.Location = New-Object System.Drawing.Point(10,190)
+$labelResult.Size = New-Object System.Drawing.Size(560,120)
+$labelResult.Font = New-Object System.Drawing.Font($labelResult.Font.FontFamily, 20)
+$mainForm.Controls.Add($labelResult)
+$ReplayButton = New-Object System.Windows.Controls.Button
+$ReplayButton.Content = "Encore"
+$ReplayButton.Visibility = "Collapsed"  # Cache le bouton jusqu'à ce que le jeu soit terminé
+$StackPanel.AddChild($ReplayButton)  # Ajoutez cette ligne après la création du StackPanel et avant l'affichage de la première question.
+
+$ReplayButton.Add_Click({
+    $script:score = 0
+    $script:count = 0
+    $TextBox.IsEnabled = $true
+    $ReplayButton.Visibility = "Collapsed"
+    New-Question
+})
+
+# Démarrer le premier tour et afficher le formulaire
+New-Round
+$mainForm.ShowDialog()
---
+++ Scripts/soustraction.ps1
@@ -0,0 +1,87 @@
+﻿# Charger les assemblies nécessaires
+Add-Type -AssemblyName PresentationFramework
+
+# Initialisation des variables
+$score = 0
+$count = 0
+
+# Fonction pour générer une nouvelle question
+Function New-Question {
+    $script:count++
+    if($script:count -le 10) {
+        $tempNum1 = Get-Random -Minimum 0 -Maximum 20
+        $tempNum2 = Get-Random -Minimum 0 -Maximum 20
+        # Assurez-vous que num1 est toujours le plus grand
+        if ($tempNum1 -ge $tempNum2) {
+            $script:num1 = $tempNum1
+            $script:num2 = $tempNum2
+        } else {
+            $script:num1 = $tempNum2
+            $script:num2 = $tempNum1
+        }
+        $Label.Content = "$script:num1 - $script:num2 ?"
+        $TextBox.Text = ""
+        $TextBox.Focus()
+    } else {
+        $Label.Content = "Final Score: $script:score/10"
+        $TextBox.IsEnabled = $false
+        $ReplayButton.Visibility = "Visible"  # Affiche le bouton lorsque le jeu est terminé
+    }
+}
+
+# Fonction pour vérifier la réponse
+Function Check-Answer {
+    $answer = [int]$TextBox.Text
+    $correctAnswer = $script:num1 - $script:num2
+    if($answer -eq $correctAnswer) {
+        $script:score++
+        $Result.Content = "Correct!"
+    } else {
+        $Result.Content = "Incorrect! La réponse à $script:num1 - $script:num2 était $correctAnswer et non $answer."
+    }
+    New-Question
+}
+
+# Création de la fenêtre
+$Window = New-Object System.Windows.Window
+$Window.Title = "Addition"
+$Window.Width = 400  # Largeur de la fenêtre
+$Window.Height = 300  # Hauteur de la fenêtre
+$Window.MinWidth = 300  # Largeur minimale
+$Window.MinHeight = 200  # Hauteur minimale
+$Window.ResizeMode = "CanResizeWithGrip"
+
+# Création des contrôles
+$StackPanel = New-Object System.Windows.Controls.StackPanel
+$Label = New-Object System.Windows.Controls.Label
+$TextBox = New-Object System.Windows.Controls.TextBox
+$TextBox.Add_KeyDown({
+    if ($_.Key -eq "Return") {
+        Check-Answer
+    }
+})
+$Result = New-Object System.Windows.Controls.Label
+
+# Ajout des contrôles à la fenêtre
+$StackPanel.AddChild($Label)
+$StackPanel.AddChild($TextBox)
+$StackPanel.AddChild($Result)
+$Window.Content = $StackPanel
+$ReplayButton = New-Object System.Windows.Controls.Button
+$ReplayButton.Content = "Encore"
+$ReplayButton.Visibility = "Collapsed"  # Cache le bouton jusqu'à ce que le jeu soit terminé
+$StackPanel.AddChild($ReplayButton)  # Ajoutez cette ligne après la création du StackPanel et avant l'affichage de la première question.
+
+$ReplayButton.Add_Click({
+    $script:score = 0
+    $script:count = 0
+    $TextBox.IsEnabled = $true
+    $ReplayButton.Visibility = "Collapsed"
+    New-Question
+})
+
+# Affichage de la première question
+New-Question
+
+# Affichage de la fenêtre
+$Window.ShowDialog()
