1b74469228fd6b8a3bf689d0a8e5ebd075c614a6
Jan Nijdam <nijdam.jan@gmail.com>
2023-10-31 13:05:05 +0100

    addition

--- addition.ps1
+++ addition.ps1
@@ -4,41 +4,42 @@ Add-Type -AssemblyName PresentationFramework
 # Initialisation des variables
 $score = 0
 $count = 0
-$letters = 65..90 + 97..122 | ForEach-Object { [char]$_ }
 
 # Fonction pour générer une nouvelle question
 Function New-Question {
     $script:count++
     if($script:count -le 10) {
-        $script:randomLetter = $letters | Get-Random
-        $Label.Content = $script:randomLetter
+        $script:num1 = Get-Random -Minimum 0 -Maximum 100
+        $script:num2 = Get-Random -Minimum 0 -Maximum 100
+        $Label.Content = "$script:num1 + $script:num2 ?"
         $TextBox.Text = ""
         $TextBox.Focus()
     } else {
-        $Label.Content = "Votre score est $script:score sur 10."
+        $Label.Content = "Final Score: $script:score/10"
         $TextBox.IsEnabled = $false
     }
 }
 
 # Fonction pour vérifier la réponse
 Function Check-Answer {
-    $answer = $TextBox.Text
-    $vowels = "AEIOUaeiou"
-    if($vowels -contains $script:randomLetter -and $answer -eq "1" -or
-       $vowels -notcontains $script:randomLetter -and $answer -eq "2") {
+    $answer = [int]$TextBox.Text
+    $correctAnswer = $script:num1 + $script:num2
+    if($answer -eq $correctAnswer) {
         $script:score++
         $Result.Content = "Correct!"
     } else {
-        $Result.Content = "Incorrect!"
+        $Result.Content = "Incorrect! La réponse à $script:num1 + $script:num2 était $correctAnswer et non $answer."
     }
     New-Question
 }
 
 # Création de la fenêtre
 $Window = New-Object System.Windows.Window
-$Window.Title = "Voyelle ou Consonne"
-$Window.Width = 400
-$Window.Height = 300
+$Window.Title = "Addition"
+$Window.Width = 400  # Largeur de la fenêtre
+$Window.Height = 300  # Hauteur de la fenêtre
+$Window.MinWidth = 300  # Largeur minimale
+$Window.MinHeight = 200  # Hauteur minimale
 $Window.ResizeMode = "CanResizeWithGrip"
 
 # Création des contrôles
@@ -46,11 +47,7 @@ $StackPanel = New-Object System.Windows.Controls.StackPanel
 $Label = New-Object System.Windows.Controls.Label
 $TextBox = New-Object System.Windows.Controls.TextBox
 $TextBox.Add_KeyDown({
-    if ($_.Key -eq "NumPad1" -or $_.Key -eq "D1") {
-        $TextBox.Text = "1"
-        Check-Answer
-    } elseif ($_.Key -eq "NumPad2" -or $_.Key -eq "D2") {
-        $TextBox.Text = "2"
+    if ($_.Key -eq "Return") {
         Check-Answer
     }
 })
